ff3cee572d5e22204e6201c6274700d14c3c4f97
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-05-26 09:40:19 -0700

    imagenes ahora si y modificaciones

--- recetas_app/views.py
+++ recetas_app/views.py
@@ -184,13 +184,9 @@ def add_to_cart(request, product_id):
     product = get_object_or_404(Recetas, id=product_id)
 
     cart_item, created = RecetaF.objects.get_or_create(
-        cart=cart,
-        product=product
+        favorito=cart,
+        recetas=product
     )
-
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
 
     return redirect('cart_detail')
 
